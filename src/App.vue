e35821480e1cc581cfbe37d570fe87cca13eee66
GarfDev <garfdev.13@gmail.com>
2022-09-05 19:16:36 +0700

    feat: init client flow

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <p>{{ client.session.cached.user }}</p>
+  <HelloWorld msg="Welcome to Your Vue.js App" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { THXClient } from "@thxprotocol/sdk";
+import HelloWorld from "./components/HelloWorld.vue";
+
+const client = new THXClient({
+  clientId: "2M07qh8c16Eu_e7f78md5",
+  clientSecret:
+    "m301KSeyeRFwRHxRfOnYMFD6vFDL5pndfT96XEgASVU6MWM40xM48HqSnRDM8xuD2S4tv1Yzx0ZDYeE3fd5Pvg",
+  grantType: "authorization_code",
+  redirectUrl: "http://localhost:8080/signin-oidc",
+});
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+  data() {
+    return {
+      client,
+    };
+  },
+
+  async mounted() {
+    const authenticated = await client.init();
+    if (!authenticated) await client.signin();
+  },
+};
 </script>
 
 <style>
--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,20 @@
-const { defineConfig } = require('@vue/cli-service')
+const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
-  transpileDependencies: true
-})
+  transpileDependencies: true,
+  configureWebpack: {
+    resolve: {
+      fallback: {
+        buffer: "buffer",
+        crypto: require.resolve("crypto-browserify"),
+        stream: require.resolve("stream-browserify"),
+        assert: require.resolve("assert/"),
+        http: require.resolve("stream-http"),
+        https: require.resolve("https-browserify"),
+        url: require.resolve("url/"),
+      },
+      alias: {
+        buffer: "buffer",
+      },
+    },
+  },
+});
